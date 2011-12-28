c22a71df0e5b78512caea99d7c06447ad52e2093
maxfoundry <maxfoundry@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-28 03:55:03 +0000

    Added tag for 1.3.2
    git-svn-id: https://plugins.svn.wordpress.org/maxbuttons/tags/1.3.2@481364 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/maxbuttons-pro.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+
+<div id="maxbuttons">
+	<div class="wrap">
+		<div class="icon32">
+			<a href="http://maxbuttons.com" target="_blank"><img src="<?php echo MAXBUTTONS_PLUGIN_URL ?>/images/mb-32.png" alt="MaxButtons" /></a>
+		</div>
+		
+		<h2 class="title">MaxButtons: Go Pro</h2>
+		
+		<div class="logo">
+			Brought to you by
+			<a href="http://maxfoundry.com" target="_blank"><img src="<?php echo MAXBUTTONS_PLUGIN_URL ?>/images/max-foundry.png" alt="Max Foundry" /></a>
+		</div>
+		
+		<div class="clear"></div>
+		
+		<h2 class="tabs">
+			<span class="spacer"></span>
+			<a class="nav-tab" href="<?php echo admin_url() ?>admin.php?page=maxbuttons-controller&action=list">Buttons</a>
+			<a class="nav-tab nav-tab-active" href="">Go Pro</a>
+		</h2>
+		
+		<div class="pro-offer">
+			<p>Everyone loves buttons, including you apparently.</p>
+			
+			<p>If you've created at least one button with MaxButtons, then you know what it can do: Unlimited colors. Rounded corners. Gradients. Text shadowing. Box shadowing. CSS3 that degrades gracefully.</p>
+			
+			<p>Those things are great, but what if it could do more? For example:</p>
+			
+			<ul>
+				<li>Support for two lines of text instead of just one.</li>
+				<li>Packs of pre-defined buttons to use as-is or to tweak and customize as you see fit.</li>
+				<li>A large selection of Google Web Fonts for beautiful typography.</li>
+				<li>Import and export functionality, a huge time-saver.</li>
+			</ul>
+			
+			<p>Soon you can have all that and more with <strong>MaxButtons Pro</strong>.</p>
+			
+			<p>And for anyone using the free version of MaxButtons (you), we're offering the Pro version for <strong>HALF OFF</strong>.</p>
+			
+			<p>All you have to do is enter your email address below and we'll send you the discount code as soon as MaxButtons Pro is released.</p>
+			
+			<form action="http://maxfoundry.createsend.com/t/r/s/vuubd/" method="post" id="subForm">
+				Email: <input type="text" name="cm-vuubd-vuubd" id="vuubd-vuubd" />
+				<input type="submit" value="Sign Me Up" class="button-primary" />
+			</form>
+		</div>
+	</div>
+</div>
