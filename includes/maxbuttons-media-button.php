73009cb92bf4ddd48a5abc0c49a758638f49527f
johnbhartley <johnbhartley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 07:47:46 +0000

    1.28.1 Tags, updated CSS
    git-svn-id: https://plugins.svn.wordpress.org/maxbuttons/tags/1.28.1@1032976 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/maxbuttons-media-button.php
@@ -0,0 +1,48 @@
+<?php
+global $pagenow;
+?>
+
+<?php // Only run in post/page creation and edit screens ?>
+<?php if (in_array($pagenow, array('post.php', 'page.php', 'post-new.php', 'post-edit.php'))) { ?>
+	<?php $published_buttons = maxbuttons_get_published_buttons(); ?>
+	
+	<script type="text/javascript">
+		function insertButtonShortcode(button_id) {
+			if (button_id == "") {
+				alert("<?php _e('Please select a button.', 'maxbuttons') ?>");
+				return;
+			}
+			
+			// Send shortcode to the editor
+			window.send_to_editor('[maxbutton id="' + button_id + '"]');
+		}
+	</script>
+	
+	<div id="select-maxbutton-container" style="display: none;">
+		<div class="wrap">
+			<h2 style="padding-top: 3px; padding-left: 40px; background: url(<?php echo MAXBUTTONS_PLUGIN_URL . '/images/mb-32.png' ?>) no-repeat;">
+				<?php _e('Insert Button into Editor', 'maxbuttons') ?>
+			</h2>
+
+			<p><?php _e('Select a button from the list below to place the button shortcode in the editor.', 'maxbuttons') ?></p>
+			
+			<table cellpadding="10" cellspacing="0" width="100%">
+			<?php foreach ($published_buttons as $button) { ?>
+				<tr>
+					<td style="border-bottom: 1px solid #ccc; padding: 20px 0;">
+						<a href="#" onclick="insertButtonShortcode(<?php echo $button->id ?>); return false;"><?php _e('Insert This Button', 'maxbuttons') ?></a> <span class="raquo">&raquo;</span>
+						<?php if($button->description != '') {
+							echo '<p style="margin: 0; font-size: 11px;"><strong>Description: </strong> ' . $button->description . '</p>';
+						} ?>
+					</td>
+					<td style="border-bottom: 1px solid #ccc; padding: 20px 0;">
+						<?php echo do_shortcode('[maxbutton id="' . $button->id . '" externalcss="false" ignorecontainer="true"]') ?>
+					</td>
+				</tr>
+			<?php } ?>
+			</table>
+
+			<a class="button-secondary" style="margin-left: 10px; margin-top: 10px;" onclick="tb_remove();"><?php _e('Cancel', 'maxbuttons') ?></a>
+		</div>
+	</div>
+<?php } ?>
